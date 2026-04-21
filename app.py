220970f6582f60bc23237e9398753e49e8a06f3e
Ash <ashwiniv3124@gmail.com>
2026-04-21 16:18:10 +0530

    Initial

---
+++ app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import joblib
+import pandas as pd
+
+model = joblib.load("model.pkl")
+
+st.title("🍷 Wine Quality Predictor")
+
+st.write("Enter values:")
+
+# Inputs (MATCH your dataset columns EXACTLY)
+fixed_acidity = st.number_input("fixed acidity")
+volatile_acidity = st.number_input("volatile acidity")
+citric_acid = st.number_input("citric acid")
+residual_sugar = st.number_input("residual sugar")
+chlorides = st.number_input("chlorides")
+free_sulfur_dioxide = st.number_input("free sulfur dioxide")
+total_sulfur_dioxide = st.number_input("total sulfur dioxide")
+density = st.number_input("density")
+pH = st.number_input("pH")
+sulphates = st.number_input("sulphates")
+alcohol = st.number_input("alcohol")
+
+if st.button("Predict"):
+
+    input_data = pd.DataFrame([[
+        fixed_acidity, volatile_acidity, citric_acid,
+        residual_sugar, chlorides, free_sulfur_dioxide,
+        total_sulfur_dioxide, density, pH,
+        sulphates, alcohol
+    ]], columns=[
+        'fixed acidity','volatile acidity','citric acid',
+        'residual sugar','chlorides','free sulfur dioxide',
+        'total sulfur dioxide','density','pH',
+        'sulphates','alcohol'
+    ])
+
+    prediction = model.predict(input_data)[0]
+
+    st.success(f"Predicted Quality: {prediction}")
